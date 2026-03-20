bcb3700970bbe2b9c8af2005fc453f4b2a38d0f0
OG - York <151065930+FNC512@users.noreply.github.com>
2026-03-20 20:11:18 +0000

    password changing

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('sign-up/', v.signup, name='signup'),
     path('login/', v.loginuser, name='login'),
     path('logout/', v.logoutuser, name='logout'),
-    path('changepassword/', auth_views.PasswordResetView.as_view(template_name='changepassword.html',success_url='/login/'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='passwordchanged.html', success_url='/login/'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='sendresetemail.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='resetlinksent.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='changepassword.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='passwordchanged.html'), name='password_reset_complete'),
 ]
