3941482ab05c0c00cfc1ee9e71a2b8388c6b9692
OG - York <151065930+FNC512@users.noreply.github.com>
2026-03-20 19:12:44 +0000

    switching to correct url

--- mysite/main/static/mapfunction.js
+++ mysite/main/static/mapfunction.js
@@ -4,7 +4,7 @@ const currentMarkers = [];
 
 
 const map = L.map("map").setView([0,0], 2);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
